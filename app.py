18108d9760f08e21844278b8e0f3541db6183a19
Your Name <you@example.com>
2025-04-15 06:25:35 -0300

    Implementing CRUD of tasks & updating

--- app.py
+++ app.py
@@ -1,14 +1,90 @@
-from flask import Flask
+# SOMENTE UMA BASE PARA O ENTENDIMENTO
 
+# from flask import Flask
+
+# app = Flask(__name__)
+
+# @app.route("/")
+# def hello_world():
+#     return("Hello World!")
+
+# @app.route("/about")
+# def about():
+#     return("Pagina sobre")
+
+# if __name__ == "__main__":  
+#     app.run(debug=True)
+
+# ________________________________________________________________
+
+# CRUD - Create, Read, Update,  Delete
+
+from flask import Flask, request, jsonify
+from models.task import task
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return("Hello World!")
+# Tabela: Tarefa
 
-@app.route("/about")
-def about():
-    return("Pagina sobre")
+tasks = []
+task_id_control = 1
+
+@app.route('/tasks', methods=['POST'])
+def create_task():
+    global task_id_control
+    data = request.get_json()
+    new_task = task(id=task_id_control, title=data["title"], description=data.get("description", ""))
+    task_id_control += 1
+    tasks.append(new_task)
+    print(tasks)
+    return jsonify({"message": "Nova tarefa criada com sucesso!"})
+
+@app.route('/tasks', methods=['GET'])
+
+def get_tasks():
+    task_list = [task.to_dict() for task in tasks]
+    output = {
+                 "tasks": task_list,
+                "total_tasks": len(task_list)
+            }
+    return jsonify(output)
+
+@app.route('/tasks/<int:id>', methods=['GET'])
+def get_task(id):
+    for t in tasks:
+        if t.id == id:
+            return jsonify(t.to_dict())
+    return jsonify({"message": "Nao foi possivel encontrar a atividade"}), 404
+
+@app.route('/tasks/<int:id>', methods=["PUT"])
+def update_task(id):
+    task = None
+    for t in tasks:
+        if t.id == id:
+            task = t
+    print(task)
+
+    if task == None:
+        return jsonify({"message": "Nao foi possivel encontrar a atividade"}), 404
+    
+    data = request.get_json()
+    task.title = data["title"]
+    task.description = data["description"]
+    task.completed = data["completed"]
+    print(task)
+    return jsonify({"message": "Tarefa atualizada com sucesso"})
+
+@app.route('/tasks/<int:id>', methods=["DELETE"])
+def delete_task(id):
+    task = None
+    for t in tasks:
+        if t.id == id:
+            task = t
+
+    if not task:
+        return jsonify({"message": "Nao foi possivel encontrar a atividade"}), 404
+
+    tasks.remove(task)
+    return jsonify ({"message": "Tarefa deletada com suceso"})
 
 if __name__ == "__main__":  
     app.run(debug=True)
